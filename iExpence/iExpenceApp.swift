828f6b0c70e910264e0b33f41c6c4a3e9f9dfbbc
Jay Bhensdadia <jayvpatel29@gmail.com>
2023-03-28 14:04:14 +0530

    Initial Commit

---
+++ iExpence/iExpenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenceApp.swift
+//  iExpence
+//
+//  Created by Jay Bhensdadia on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
